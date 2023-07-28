b368cf13be960cb46146b7fad8194fd722268d7f
David Fernández Esteban <david@misei.dev>
2023-07-28 11:41:25 +0200

    Mail service change

--- src/main/java/dev/misei/application/MailService.java
+++ src/main/java/dev/misei/application/MailService.java
@@ -1,22 +1,40 @@
 package dev.misei.application;
 
 import lombok.AllArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Component
 @AllArgsConstructor
 public class MailService {
 
-    private JavaMailSender emailSender;
+    private RestTemplate restTemplate;
 
     public void sendConfirmationMessage(String to, String text) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("noreply@baeldung.com");
-        message.setTo(to);
-        message.setSubject("Your login confirmation");
-        message.setText(text);
-        emailSender.send(message);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.add("Authorization", "Bearer $SENDGRID_API_KEY");
+
+        var builder = UriComponentsBuilder.fromHttpUrl("https://api.sendgrid.com/v3/mail/send");
+        // Create the JSON payload for the email
+        var payload = "{"
+                + "\"personalizations\": [{\"to\": [{\"email\": \"" + to + "\"}]}],"
+                + "\"from\": {\"email\": \"test@example.com\"},"
+                + "\"subject\": \"Sending with SendGrid is Fun\","
+                + "\"content\": [{\"type\": \"text/plain\", \"value\": \"" + text + "\"}]"
+                + "}";
+
+        HttpEntity<?> entity = new HttpEntity<>(payload, headers);
+
+        HttpEntity<String> response = restTemplate.exchange(
+                builder.toUriString(),
+                HttpMethod.POST,
+                entity,
+                String.class);
     }
 }

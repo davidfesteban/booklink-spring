24c524b6b13ba4275add6956e5d766b0d28b4b49
David Fernández Esteban <david@misei.dev>
2023-07-28 16:01:19 +0200

    Mail Service Working

--- src/main/java/dev/misei/application/MailService.java
+++ src/main/java/dev/misei/application/MailService.java
@@ -1,10 +1,7 @@
 package dev.misei.application;
 
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
+import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -18,23 +15,29 @@ public class MailService {
     public void sendConfirmationMessage(String to, String text) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.add("Authorization", "Bearer $SENDGRID_API_KEY");
+        headers.add("Authorization", "Bearer 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");
 
         var builder = UriComponentsBuilder.fromHttpUrl("https://api.sendgrid.com/v3/mail/send");
         // Create the JSON payload for the email
         var payload = "{"
                 + "\"personalizations\": [{\"to\": [{\"email\": \"" + to + "\"}]}],"
-                + "\"from\": {\"email\": \"test@example.com\"},"
+                + "\"from\": {\"email\": \"noreply@booklink.app\"},"
                 + "\"subject\": \"Sending with SendGrid is Fun\","
-                + "\"content\": [{\"type\": \"text/plain\", \"value\": \"" + text + "\"}]"
+                + "\"content\": ["
+                + "{\"type\": \"text/html\", \"value\": \"<html>Hi from Booklink!<br>This is your confirmation link: <a href='" + text + "'>" + text + "</a><br><br>Please, do not reply to this email. If this email is not intended for you, remove it.<br>Thanks!</html>\"}"
+                + "]"
                 + "}";
 
         HttpEntity<?> entity = new HttpEntity<>(payload, headers);
 
-        HttpEntity<String> response = restTemplate.exchange(
+        ResponseEntity<String> response = restTemplate.exchange(
                 builder.toUriString(),
                 HttpMethod.POST,
                 entity,
                 String.class);
+
+        if (!response.getStatusCode().is2xxSuccessful()) {
+            throw new RuntimeException("Failed Mail Service");
+        }
     }
 }
